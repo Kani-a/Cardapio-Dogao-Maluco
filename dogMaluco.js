b558051b4679cc28c1ba0c69fcae9441b165f57a
kani-a <allanchagasilva@gmail.com>
2025-09-15 10:49:59 -0300

    Updates

---
+++ dogMaluco.js
@@ -0,0 +1,110 @@
+export const hotdogs = [
+    {
+      "nome": "Pastor alemão",
+      "salcichas": 2,
+      "complementos": [
+        "Pure",
+        "Milho",
+        "Batata Palha",
+        "Parmesão"
+      ],
+      "preço": 13.99
+    },
+    {
+      "nome": "Bulldogue",
+      "salcichas": 2,
+      "complementos": [
+        "Pure",
+        "Ervilha",
+        "Queijo Ralado",
+        "Carne Moída",
+        "Batata Palha"
+      ],
+      "preço": 18.00
+    },
+    {
+      "nome": "Labrador",
+      "salcichas": 3,
+      "complementos": [
+        "Pure",
+        "Milho",
+        "Ervilha",
+        "Batata Palha",
+        "Carne Moída",
+        "Cheddar"
+      ],
+      "preço": 21.99
+    },
+    {
+      "nome": "Pincher",
+      "salcichas": 1,
+      "complementos": [
+        "Pure",
+        "Vinagrete",
+        "Batata Palha",
+        "Frango",
+        "Queijo Ralado"
+      ],
+      "preço": 15.99
+    },
+    {
+      "nome": "Vira Latas",
+      "salcichas": 1,
+      "complementos": [
+        "Pure",
+        "Batata Palha",
+        "Vinagrete"
+      ],
+      "preço": 9.99
+    },
+    {
+      "nome": "Pit Bull ",
+      "salcichas": 3,
+      "complementos": [
+        "Ovo codorna",
+        "Pure",
+        "Cheddar",
+        "Carne Moída",
+        "Milho",
+        "Bacon"
+      ],
+      "preço": 25.00
+    },
+    {
+      "nome": "RottWeiller",
+      "salcichas": 2,
+      "complementos": [
+        "Pure",
+        "Catupiry",
+        "Frango",
+        "Vinagrete"
+      ],
+      "preço": 20.00
+    },
+     {
+      "nome": "Salsichinha",
+      "salcichas": 1,
+      "linguiça": 1,
+      "complementos": [
+        "Pure",
+        "Batata Palha",
+        "Catupiry",
+        "Carne Moída"
+      ],
+      "preço": 22.00
+    },
+    {
+      "nome": "Golden",
+      "linguiça": 2,
+      "complementos": [
+        "Carne Moída",
+        "Frango",
+        "Bacon",
+        "Cheddar",
+        "Pure",
+        "Batata Palha"
+      ],
+      "preço": 30.00
+    }
+  ]
+
