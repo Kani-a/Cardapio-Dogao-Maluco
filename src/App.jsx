8b177208e9d41288f5300b24356c73f016284caf
kani-a <allanchagasilva@gmail.com>
2025-09-14 21:55:27 -0300

    minoor changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import { hotdogs } from "../public/dogMaluco"
 import HotdogRows from "./assets/components/foodrow/hotdogSection/HotdogSection";
 import "./app.css"
+import { Image } from "antd";
 
 function App() { 
   return (
     <>
-    <h1>Dogão maluco</h1>
       <HotdogRows hotdog={hotdogs}/>
     </>
   )
--- src/assets/components/foodrow/hotdogSection/HotdogSection.jsx
+++ src/assets/components/foodrow/hotdogSection/HotdogSection.jsx
@@ -1,18 +1,28 @@
-import { List } from "antd";
+import { Image, List } from "antd";
 import './HotdogSection.css';
 
 export default function HotdogRows({ hotdog }) {
    return (
     <div className="hotdog-list-container">
+         <Image
+         preview={false}
+        src="/dogao.png"
+        />
         <List
-            header={<h1>Hotdogs</h1>}
+            header={
+                 <h1>hotdog</h1>
+                }
             bordered
             dataSource={hotdog}
             renderItem={(item) => (
                 <List.Item>
                     <List.Item.Meta
                         title={<><h2>{item.nome} </h2> <div className="preco">{item.preço.toFixed(2)} R$</div> </>}
-                        description={`Complementos: ${item.complementos.join(', ')}`}
+                         description={
+                            `Complementos: ${item.complementos.join(', ')}`
+                            + (item.salcichas ? ` | Salsichas: ${item.salcichas}` : '')
+                            + (item.linguiça ? ` | Linguiças: ${item.linguiça}` : '')
+                        }
                     />
                     
                 </List.Item>
