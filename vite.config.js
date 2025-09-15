d1381176809b4bfa67b9326245e084b461170045
kani-a <allanchagasilva@gmail.com>
2025-09-15 10:55:54 -0300

    base repo vite.config fix

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: 'https://github.com/Kani-a/Cardapio-Dogao-Maluco.git', // 👈 troque para o nome do seu repo
+  base: '/Cardapio-Dogao-Maluco/'
 })
