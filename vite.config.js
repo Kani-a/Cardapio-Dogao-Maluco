5837f73e85b4c6fc14522700c975b505500676f3
kani-a <allanchagasilva@gmail.com>
2025-09-15 10:50:33 -0300

    deploy

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: 'https://github.com/Kani-a/Cardapio-Dogao-Maluco.git', // 👈 troque para o nome do seu repo
 })
